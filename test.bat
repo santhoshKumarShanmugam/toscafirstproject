fee6bb77ae0e61453e46d25b18f03fb44ef8aca8
Shanmugam <S.Shanmugam3@shell.com>
2024-12-18 17:19:15 +0530

    test batch committed

---
+++ test.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Navigate to the Tosca project directory
+cd C:\Users\S.Shanmugam3\Tosca_Projects\Tosca_Workspaces\multiuser_con_git
+
+REM Run the Tosca test case
+tosca testsuite:run --project "C:\Users\S.Shanmugam3\Tosca_Projects\Tosca_Workspaces\multiuser_con_git" --testsuite "Excel Engine"
