6851cf7b9f3111e44811de5aa790ca56e3b13b9f
Shanmugam <S.Shanmugam3@shell.com>
2024-12-18 18:02:17 +0530

    path updated

--- test.bat
+++ test.bat
@@ -1,3 +1,8 @@
 @echo off
-cd C:\Users\S.Shanmugam3\Tosca_Projects\Tosca_Workspaces\multiuser_con_git
+cd C:
+cd Users
+cd S.Shanmugam3
+cd Tosca_Projects
+cd Tosca_Workspaces
+cd multiuser_con_git
 tosca testsuite:run --project "C:\Users\S.Shanmugam3\Tosca_Projects\Tosca_Workspaces\multiuser_con_git" --testsuite "Excel Engine"
